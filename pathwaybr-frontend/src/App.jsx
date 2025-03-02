6ea1b538572e6c798755c493d791d8992af2f9cb
gustavo melo <gustavo.melo@enjoei.com.br>
2025-03-02 20:22:12 -0300

    :feat: add frontend

---
+++ pathwaybr-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/header"
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <h1>PathwayBR</h1>
+      <p>Seu sistema de viagem</p>
+    </div>
+  )
+}
+
+export default App
---
+++ pathwaybr-frontend/src/components/header.jsx
@@ -0,0 +1,17 @@
+import "./Header.css"
+function Header() {
+    return (
+      <header>
+        <h1>PathwayBR</h1>
+        <nav>
+          <ul>
+            <li><a href="#">Início</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Contato</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Header;
